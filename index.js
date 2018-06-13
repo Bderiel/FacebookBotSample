a515eeb1eaf7cad36f3cc2b09e5fa2e65e51d614
Bderiel <bderiel@gmail.com>
2018-06-13 15:10:32 -0400

    added morgan for server logs

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const morgan = require('morgan');
 
 const app = express().use(bodyParser.json());
 const PORT = process.env.PORT || '1337';
 
+app.use(morgan('dev'));
 app.use('/webhook', require('./webhook'));
 
 app.listen(PORT, () => console.log(`Listening of ${PORT}`));
